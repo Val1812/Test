1cef08fb1a32a080abf99a27ed7313117d9c4770
anerol55555 <leoncita_11_13@hotmail.com>
2016-01-27 17:15:22 -0600

    code impruved

--- hola.js
+++ hola.js
@@ -1,6 +1,6 @@
-console.log('Hola mundo');
-
-var x = 10,
- 	y = 20;
-
- console.log(x + y);
+var array = [1,2,3,4];
+var result = 0;
+array.forEach(function (element){
+	result = result + element;
+});
+console.log(result);
